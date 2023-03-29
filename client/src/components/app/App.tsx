6f9573a0e4ab4d008cb0b63b501ac0bd63eca997
Fuko <fukodiaz@gmail.com>
2023-03-29 21:41:40 +0700

    first set up

---
+++ client/src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+
+import styles from './App.module.less';
+
+const App:FC = () => {
+
+	return (
+		<div className={styles.mainWrapper}>
+			first
+		</div>
+	);
+}
+
+export default App;
---
+++ client/src/globals.d.ts
@@ -0,0 +1,21 @@
+// declare module "*.module.css";
+// declare module "*.module.less";
+// declare module '*.m.less' {
+// 	const classes: {[key: string]: string};
+// 	export default classes;
+//  }
+
+// declare module "*.css" {
+// 	export default {
+// 	  [index: string]: string;
+// 	}
+//  }
+
+// declare module '*.module.less' {
+// 	const classes: { readonly [key: string]: string };
+// 	export default classes;
+// }
+declare module '*.less';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.svg';
