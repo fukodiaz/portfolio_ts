9a3c987f63c3c7cbb3ee63b587b3e46eaaf63120
Fuko <fukodiaz@gmail.com>
2023-04-05 22:24:17 +0700

    styling block with jeneral info

--- client/src/components/jeneral-info/jeneral-info.tsx
+++ client/src/components/jeneral-info/jeneral-info.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react';
 
+import {jeneralInfoData, TypeJeneralInfo} from '../../data/generalInfoData';
 import {TbSquareRotatedFilled as Rhomb} from 'react-icons/tb';
+import imageJenInfo from './imageJenInfo.svg';
 import styles from './jeneral-info.module.less';
 
 const JeneralInfo: FC = () => {
@@ -10,6 +12,18 @@ const JeneralInfo: FC = () => {
 			<div className={styles.boxLineDecoration}>
 				<Rhomb className={styles.rhomb} />
 				<p className={styles.line}></p>
+			</div>
+			<div className={styles.wrapperContentInfo}>
+				<div className={styles.contentInfo}>
+					<h3 className={styles.titleJeneralInfo}>{jeneralInfoData.title}</h3>
+					<div className={styles.descripJeneralInfo}>
+						{jeneralInfoData.descriptionFirst} <br />
+						{jeneralInfoData.descriptionSecond}
+					</div>
+				</div>
+				<div className={styles.boxImageJeneralInfo}>
+					<img src={imageJenInfo} alt='' />
+				</div>
 			</div>
 		</section>
 	);
---
+++ client/src/data/generalInfoData.tsx
@@ -0,0 +1,11 @@
+export type TypeJeneralInfo = {
+	title: string,
+	descriptionFirst: string,
+	descriptionSecond: string
+}
+
+export const jeneralInfoData: TypeJeneralInfo = {
+	title: 'Немного обо мне',
+	descriptionFirst: 'Такой-то, такой-то. Там-то, там-то.',
+	descriptionSecond: 'На сегодняшний день здесь.'
+}
