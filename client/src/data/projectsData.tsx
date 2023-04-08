724d05174b8310c94df9c27ca50d729a8e40218b
Fuko <fukodiaz@gmail.com>
2023-04-08 22:37:11 +0700

    fill data for list of projects

--- client/src/data/projectsData.tsx
+++ client/src/data/projectsData.tsx
@@ -1,5 +1,8 @@
 import bel_go from './png/bel_go.png'
 import admin from './png/admin.png'
+import tasks from './png/tasks.png'
+import table from './png/table.png'
+import sedona from './png/sedona.png'
 
 export type TypeProject = {
 	id: number | string,
@@ -31,12 +34,30 @@ export const projectsData: TypeProject[] = [
 		image: admin
 	},
 	{
-		id: 2,
-		title: 'Admin-account',
-		description: 'Учебный проект, подразумевающий создание личного кабинета администратора системы',
-		elsStack: ['React', 'Redux', 'Less', 'Node.js', 'Redis'],
-		code: 'https://github.com/fukodiaz/admin_account',
-		deploy: 'https://admin-account.vercel.app/',
-		image: admin
+		id: 3,
+		title: 'Tasks',
+		description: 'Приложение, позволяющее создавать, редактировать, распределять задачи в рамках определенных проектов',
+		elsStack: ['React', 'Redux', 'SCSS'],
+		code: 'https://github.com/fukodiaz/tasks_test',
+		deploy: 'https://fukodiaz.github.io/tasks_test/',
+		image: tasks
+	},
+	{
+		id: 4,
+		title: 'Table',
+		description: 'Выполненное мною тестовое задание, предполагавшее создание таблицы, отображающей данные пользователей',
+		elsStack: ['React', 'Redux', 'Less'],
+		code: 'https://github.com/fukodiaz/table_test',
+		deploy: 'https://table-test-psi.vercel.app/',
+		image: table
+	},
+	{
+		id: 5,
+		title: 'Sedona',
+		description: 'Cтатический сайт "Sedona" на основе макетов HTML Academy',
+		elsStack: ['Less', 'JavaScript'],
+		code: 'https://github.com/fukodiaz/Sedona-3version',
+		deploy: 'https://fukodiaz.github.io/Sedona-3version/',
+		image: sedona
 	}
 ]
